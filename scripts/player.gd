f6f1f7c3545b4f2312fe373455c0371d1227828d
Jwiz <notirfanbasheer@gmail.com>
2024-08-17 22:40:37 +0300

    Make hold to inflate mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,13 +7,20 @@ const JUMP_VELOCITY = -400.0
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var flag: bool = false
 
 func _physics_process(delta: float) -> void:
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		apply_central_impulse(Vector2(0,-600))
+	if Input.is_action_pressed("jump"):
 		print_debug("Test!")
-		sprite_2d.apply_scale(Vector2(1.1,1.1))
-		collision_shape_2d.apply_scale(Vector2(1.1,1.1))
-		animation_player.play("big")
-		
+		if not flag:
+			apply_central_impulse(Vector2(0,-600))
+			sprite_2d.apply_scale(Vector2(1.25,1.25))
+			collision_shape_2d.apply_scale(Vector2(1.25,1.25))
+			animation_player.play("big")
+			flag = true		
+	elif flag:
+		sprite_2d.apply_scale(Vector2(0.8,0.8))
+		collision_shape_2d.apply_scale(Vector2(0.8,0.8))
+		animation_player.play("small")
+		flag = false
