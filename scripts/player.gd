0f7b34bd17e08b80cda16db197a040e480fe6a3d
Jwiz <notirfanbasheer@gmail.com>
2024-08-17 22:20:27 +0300

    add fish

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,19 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if Input.is_action_just_pressed("jump"):
+		apply_central_impulse(Vector2(0,-600))
+		print_debug("Test!")
+		sprite_2d.apply_scale(Vector2(1.1,1.1))
+		collision_shape_2d.apply_scale(Vector2(1.1,1.1))
+		animation_player.play("big")
+		
